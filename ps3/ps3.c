b0111751e1141e2cf726b995c025f3c67378ede7
Luke de Oliveira <lukepercivaldeoliveira@gmail.com>
2013-12-07 16:16:03 -0500

    still working on seek

--- ps3/ps3.c
+++ ps3/ps3.c
@@ -133,17 +133,119 @@ typedef struct control_object
 	int children[4];
 	box Box;
 } control_object;
+
+int is_in_box(point p, box B)
+{
+	if ((p.x > B.Box.t_right.x) || (p.x < B.Box.b_left.x))
+	{
+		return 0;
+	}
+	if ((p.y > B.Box.t_right.y) || (p.y < B.Box.b_left.y))
+	{
+		return 0;
+	}
+	return 1;
+}
+
+void arr_to_points(double *a, point *points, int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		points[i].x = a[2 * i];
+		points[i].y = a[2 * i + 1];
+	}
+}
+
+
+void print_point(point p)
+{
+	printf("(%f, %f)\n", p.x, p.y);
+}
+
+
+void gen_sub_boxes(box X, box *a, box *b, box *c, box *d)
+{
+	a->b_left.x = X.b_left.x;
+	a->b_left.y = (X.t_right.y - X.b_left.y) / 2;
+
+	a->t_right.y = X.t_right.y;
+	a->t_right.x = (X.t_right.x - X.b_left.x) / 2;
+
+	b->b_left.x = (X.t_right.x - X.b_left.x) / 2;
+	b->b_left.y = (X.t_right.y - X.b_left.y) / 2;
+
+	b->t_right.y = X.t_right.y;
+	b->t_right.x = X.t_right.x;
+
+
+	c->b_left.x = X.b_left.x;
+	c->b_left.y = X.b_left.y;
+
+	c->t_right.y = b->b_left.y;
+	c->t_right.x = b->b_left.x;
+
+	d->b_left.x = (X.t_right.x - X.b_left.x) / 2;
+	d->b_left.y = X.b_left.y;
+
+	d->t_right.y = (X.t_right.y - X.b_left.y) / 2;
+	d->t_right.x = X.t_right.x;
+}
+
+
 //----------------------------------------------------------------------------
 
 
-void add_control_entry(control_object *control, int *permutation, int current, int next, double *a, int n)
+void add_control_entry(control_object *control, int *permutation, int current, int next, point *Data, int n)
 {
-	
+	int head = control[current].start;
+	int tail = control[current].end;
+
+	int i;
+	for (i = 0; i < 4; ++i)
+	{
+		control[current].children[i] = next + i;
+		control[next + i].parent = current;
+	}
+	gen_sub_boxes(control[current].Box, &control[next].Box, 
+		                                &control[next + 1].Box, 
+		                                &control[next + 2].Box, 
+		                                &control[next + 3].Box);
+	printf("%s\n", "parent box:");
+	print_point(control[current].Box.b_left);
+	print_point(control[current].Box.t_right);
+
+	int count = 0;
+
+	while(count < 4)
+	{
+		int s = head;
+		int e = tail;
+		while(s <= e)
+		{
+			if (is_in_box(Data[permutation[s]], control[next+count].Box))
+			{
+				s++;
+			}
+			else
+			{
+				int_swap(&permutation[s], &permutation[e]);
+			}
+		}
+		control[next+count].start = head;
+		control[next+count].end = s - 1;
+		count++;
+		head = s;
+	}	
 }
 
 //----------------------------------------------------------------------------
 void seek(double *a, int n, int k, int *iz) 
 {
+	point *Data;
+	Data = malloc(n * sizeof(point));
+	arr_to_points(a, Data, n);
+
+
 	int i, j;
 	int *permutation = (int*) malloc(n * sizeof(int));
 	for (i = 0; i < n; i++)
@@ -153,13 +255,15 @@ void seek(double *a, int n, int k, int *iz)
 
 	control_object *Control;
 	Control = malloc(BUF * sizeof (control_object));
-
-	Control[0].is_processed = 1, Control[0].start = 0, Control[0].end = n - 1;
+	int current = 0;
+	Control[0].is_processed = 3, Control[0].start = 0, Control[0].end = n - 1;
 	Control[0].parent = -8, 
 	Control[0].Box.b_left.x = 0, Control[0].Box.b_left.y = 0;
 	Control[0].Box.t_right.x = 1, Control[0].Box.t_right.y = 1;
+	print_point(Control[current].Box.b_left);
+	print_point(Control[current].Box.t_right);
 
-	int current = 0;
+	
 	int next = 1;
 
 	while(current < next)
@@ -168,8 +272,19 @@ void seek(double *a, int n, int k, int *iz)
 		int s = Control[current].start;
 		if (e - s > k)
 		{
-			/* code */
+			add_control_entry(Control, permutation, current, next, Data, n);
+			next += 4;
 		}
+		Control[current].is_processed;
+		current++;
+	}
+
+
+	for (i = 0; i < 4; ++i)
+	{
+		printf("\nChild box #%d:\n", i+1);
+		print_point(Control[next+i].Box.b_left);
+		print_point(Control[next+i].Box.t_right);
 	}
 
 }
@@ -222,6 +337,7 @@ int main(int argc, char const *argv[])
 	}
 	dist_matrix(D, a, n);
 	seek_naive(a, n, k, iz);
+	seek(a, n, k, iz);
 	printf("a matrix\n");
 	print_matrix(a, n, 2);
 	printf("D matrix\n");
